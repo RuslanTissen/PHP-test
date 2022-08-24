80eff07474673a9bc9e1be77a7f8542eeadf1fca
Ruslan <rthyssen73@gmail.com>
2022-08-24 11:10:43 +0200

    index.php updated

--- public/index.php
+++ public/index.php
@@ -6,25 +6,54 @@
 // All calls are sent to this file.
 // Все вызовы направляются в этот файл.
 
-define('HLEB_START', microtime(true));
-define('HLEB_FRAME_VERSION', "1.6.69");
-define('HLEB_PUBLIC_DIR', __DIR__);
+// define('HLEB_START', microtime(true));
+// define('HLEB_FRAME_VERSION', "1.6.69");
+// define('HLEB_PUBLIC_DIR', __DIR__);
 
 
 // General headers.
 // Общие заголовки.
 
-header("Referrer-Policy: no-referrer-when-downgrade");
-header("X-XSS-Protection: 1; mode=block");
-header("X-Content-Type-Options: nosniff");
-header("X-Frame-Options: SAMEORIGIN");
+// header("Referrer-Policy: no-referrer-when-downgrade");
+// header("X-XSS-Protection: 1; mode=block");
+// header("X-Content-Type-Options: nosniff");
+// header("X-Frame-Options: SAMEORIGIN");
 // ...
 
 // Initialization.
 // Инициализация.
 
-require __DIR__ . '/../vendor/phphleb/framework/bootstrap.php';
+// require __DIR__ . '/../vendor/phphleb/framework/bootstrap.php';
 
-exit();
+
+
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=London
+// &appid=599abb33dacb212628592cd89fc30f3f
+
+
+$url = 'http://api.openweathermap.org/data/2.5/weather';
+
+$options = array(
+	'id' => '703448',
+	'appid' => '57be7c28549ba906dda991a015238f2a',
+	'units' => 'metric',
+	'lang' => 'en',
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+$response = curl_exec($ch);
+$data = json_decode($response, true);
+curl_close($ch);
+
+echo '<pre>';
+print_r($data);
+
+// exit();
 
 
